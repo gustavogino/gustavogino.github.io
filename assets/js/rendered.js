77ce2272169c1442dca3f3d0a3ae93f85079efaf
Gustavo Gino Scotton <gustavo.gino@outlook.com>
2021-01-08 22:31:47 -0300

    New features
    Include fake windows update, fake hacking and easter egg music

--- assets/js/rendered.js
+++ assets/js/rendered.js
@@ -228,3 +228,27 @@ $(document).ready(function () {
   var txt = "";
 
 });
+
+function toggleFullScreen(elem) {
+ if ((document.fullScreenElement !== undefined && document.fullScreenElement === null) || (document.msFullscreenElement !== undefined && document.msFullscreenElement === null) || (document.mozFullScreen !== undefined && !document.mozFullScreen) || (document.webkitIsFullScreen !== undefined && !document.webkitIsFullScreen)) {
+     if (elem.requestFullScreen) {
+         elem.requestFullScreen();
+     } else if (elem.mozRequestFullScreen) {
+         elem.mozRequestFullScreen();
+     } else if (elem.webkitRequestFullScreen) {
+         elem.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+     } else if (elem.msRequestFullscreen) {
+         elem.msRequestFullscreen();
+     }
+ } else {
+     if (document.cancelFullScreen) {
+         document.cancelFullScreen();
+     } else if (document.mozCancelFullScreen) {
+         document.mozCancelFullScreen();
+     } else if (document.webkitCancelFullScreen) {
+         document.webkitCancelFullScreen();
+     } else if (document.msExitFullscreen) {
+         document.msExitFullscreen();
+     }
+ }
+}
